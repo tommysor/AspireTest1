5a3efa3fe6ed66a122c4e0af4ced418ae67de1bc
Tommy Sørbråten <tommysor@gmail.com>
2024-03-02 19:10:54 +0000

    Bump to net9.0

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,6 @@
 dotnet workload restore
 dotnet tool restore
 # Install "az" (Azure cli)
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 # Install "azd" (Azure Developer cli)
-curl -fsSL https://aka.ms/install-azd.sh | bash
+#curl -fsSL https://aka.ms/install-azd.sh | bash
